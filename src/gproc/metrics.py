d82a0def1a693d9d5c3207d9a7c7efdedadf181e
tennessee_wallaceh <tennessee.wallaceh@gmail.com>
2022-05-27 12:54:06 +0100

    Fix abstension interpretation in metrics

--- src/gproc/metrics.py
+++ src/gproc/metrics.py
@@ -27,27 +27,26 @@ def predictive_metrics(prob_ests, y_ests, y_test, rho):
     thresh_inds = np.argwhere((prob_ests < 0.5 - rho) | (prob_ests > 0.5 + rho))
     
     # Work out degree of abstention
-    abstention_degree = thresh_inds.shape[0]/N
+    abstention_degree = 1 - (thresh_inds.shape[0] / N)
 
-    if abstention_degree == 0:
+    if thresh_inds.shape[0] == 0: # We make no predictions at this threshold
         raise ThresholdError("Threshold value too large, no data passes")
     
     # Get thresholded predicted and test responses, and probabilities
     y_test_threshed = y_test[thresh_inds]
     y_ests_threshed = y_ests[thresh_inds]
-    prob_ests_thresed = prob_ests[thresh_inds]
-    
-    if (np.unique(y_test_threshed).shape[0] == 1) | (np.unique(y_ests_threshed).shape[0] == 1):
-        raise ThresholdError("Threshold value too large, only one class")
+    prob_ests_threshed = prob_ests[thresh_inds]
         
     # Work out the capacity classifcication accuracy
     cap_acc_score = np.mean(y_test_threshed == y_ests_threshed)
     
-    # Work out the capacity  F score 
-    cap_f_score = f1_score(y_test_threshed, y_ests_threshed)
-    
-    # Work out the capacity  AUC score
-    cap_auc_score = roc_auc_score(y_test_threshed, prob_ests_thresed)
+    # Work out the capacity  AUC score + F score 
+    if np.unique(y_test_threshed).shape[0] == 1:
+        cap_auc_score = np.nan
+        cap_f_score = np.nan
+    else:
+        cap_auc_score = roc_auc_score(y_test_threshed, prob_ests_threshed)
+        cap_f_score = f1_score(y_test_threshed, y_ests_threshed)
     
     return abstention_degree, cap_acc_score, cap_f_score, cap_auc_score
 
@@ -64,11 +63,12 @@ def abstention_metrics(prob_ests, y_ests, y_test, rhos):
                                         accuracy capacity F score, capacity AUC score
     """
 
-    cap_metrics = np.zeros((rhos.shape[0], 4))
+    cap_metrics = np.zeros((rhos.shape[0], 5))
 
     for i in range(rhos.shape[0]):
         try:
-            cap_metrics[i, :] = predictive_metrics(prob_ests, y_ests, y_test, rho[i])
+            cap_metrics[i, :-1] = predictive_metrics(prob_ests, y_ests, y_test, rhos[i])
+            cap_metrics[i, -1] = rhos[i]
         except ThresholdError:
             return cap_metrics[0:i, ]
         
@@ -95,7 +95,7 @@ def max_threshold(prob_ests):
         
     return max_thresh
 
-def inspect_metrics(accuracy_ax, f_score_ax, auc_ax, cap_metrics):
+def inspect_metrics(accuracy_ax, auc_ax, cap_metrics, label=None):
     """
     Produces plots of the capacity accuracy, capacity F score and capacity AUC scores vs
     the degree of abstention
@@ -103,19 +103,16 @@ def inspect_metrics(accuracy_ax, f_score_ax, auc_ax, cap_metrics):
     :param cap_metrics, M x 4 matrix, matrix holding abstention degree, capacity classification
                                         accuracy capacity F score, capacity AUC score
     """
-    
-    
-    accuracy_ax.plot(cap_metrics[:, 0], cap_metrics[:, 1], label = 'Capacity Accuracy Score')
+    accuracy_ax.plot(cap_metrics[:, 0], cap_metrics[:, 1], label =label)
     accuracy_ax.set_xlabel('Degree of Abstention', fontsize = 15)
     accuracy_ax.set_ylabel('Capacity Classification Accuracy', fontsize = 15)
     accuracy_ax.set_ylim([-0.05, 1.05])
+    accuracy_ax.set_xlim([0.0, 1.])
+    accuracy_ax.legend()
     
-    f_score_ax.plot(cap_metrics[:, 0], cap_metrics[:, 2], label = 'Capacity F Score')
-    f_score_ax.set_xlabel('Degree of Abstention', fontsize = 15)
-    f_score_ax.set_ylabel('Capacity F Score', fontsize = 15)
-    f_score_ax.set_ylim([-0.05, 1.05])
-    
-    auc_ax.plot(cap_metrics[:, 0], cap_metrics[:, 3], label = 'Capacity AUC Score')
+    auc_ax.plot(cap_metrics[:, 0], cap_metrics[:, 3], label =label)
     auc_ax.set_xlabel('Degree of Abstention', fontsize = 15)
     auc_ax.set_ylabel('Capacity AUC Score', fontsize = 15)
     auc_ax.set_ylim([-0.05, 1.05])
+    auc_ax.set_xlim([0.0, 1.])
+    auc_ax.legend()
