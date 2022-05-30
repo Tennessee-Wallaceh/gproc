b3302050b9074afc4a8f0cc37b050affa6db4834
Edward Milsom <em17331@bristol.ac.uk>
2022-05-30 15:20:19 +0100

    Added docstring

--- src/gproc/pseudo_marginal_prediction.py
+++ src/gproc/pseudo_marginal_prediction.py
@@ -2,7 +2,39 @@ import numpy as np
 from scipy.stats import norm
 
 def pm_predict(new_x, X, y, Kernel_class, f_arr, th_arr, inverse_gram_arr):
+    """
+    Also known as RBF.
 
+    Parameters
+    ----------
+    new_x: Size D numpy vector
+        The new input point point to be predicted
+
+    X: P x D numpy array
+        The P training inputs of dimension D
+
+    y: Size P numpy vector
+        The P training targets
+
+    Kernel_class: Class
+        The class (not object) of the kernel the hyperparameters correspond to.
+
+    f_arr: N x P numpy array
+        The N samples of the latent function evaluated at the P training inputs.
+
+    th_arr: N x C numpy array
+        The N samples of the C hyperparameters
+
+    inverse_gram_arr: N x P x P numpy array
+        The N inverse Gram matrices we computed during the Metropolis-Hastings step,
+        stored to trade memory for speedier predictions.
+
+    Returns
+    ----------
+    pred: float
+        The probability of the inputted point being in the positive class
+    """
+    
     N = f_arr.shape[0]
 
     mean_stars = np.empty(N)
